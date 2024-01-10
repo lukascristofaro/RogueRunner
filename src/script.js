abf1d50ece29d802ade684a7e9ea87a77f2e6934
lukas <lukas.cristofaro@ynov.com>
2024-01-10 09:28:00 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,35 @@
+const config = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    type: Phaser.AUTO,
+    scene: {
+        preload : preload,
+        create : create,
+        update: update
+    }
+}
+
+let game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('policeCar1', 'assets/policeCar1.png');
+    this.load.image('policeCar2', 'assets/policeCar2.png');
+}
+
+function create() {
+    var sprite = this.add.sprite(100, 300, 'policeCar1');
+    this.anims.create({
+        key : 'policeCar',
+        frames : [
+            {key : 'policeCar1'},
+            {key : 'policeCar2'},
+        ],
+        frameRate : 2,
+        repeat: -1,
+    })
+    sprite.play('policeCar');
+}
+
+function update() {
+
+}
